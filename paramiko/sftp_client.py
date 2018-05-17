5cd1f1bf695aed81d3f1c32b39303b17588b443c
Radoslaw Garbacz <radoslaw.garbacz@xtremedatainc.com>
2018-05-17 14:42:02 -0500

    Fixed new callback result back-word compatibility

--- paramiko/sftp_client.py
+++ paramiko/sftp_client.py
@@ -568,7 +568,12 @@ class SFTPClient (BaseSFTP):
                     fr.write(data)
                     size += len(data)
                     if callback is not None:
-                        if not callback(size, file_size):
+                        call_res = callback(size, file_size):
+                        if call_res:
+                            pass
+                        elif call_res is None:
+                            pass
+                        else:
                             cont_f = False
                             abort_f = True
             finally:
